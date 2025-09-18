c7757c98aefcfaf8dd4428f1cd60afc86e0e52a4
unknown <shebajonah90@gmail.com>
2025-09-18 19:11:15 +0300

    index.js

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ app.get('/', (request, response)=>{
 app.listen(PORT, () =>{
     console.log('Server is running on port' + PORT);
 })
+
+//Http methods: GET, POST, PUT, DELETE
+
+app.get('/about', (req, res)=>{
+    res.send('This is my about page')
+})
+
+// Route parameters
+app.get('/users/:id', (req, res)=>{
+    const userId = req.params.id;
+    res.send('User ID is: ' + userId);
+})
+
+// Querry parameters
+app.get('/search', (req, res)=>{
+    const query = req.query.q;
+    res.send('You searched for' +query);
+})
+
+// Post request
+app.post('/about', (req, res) => {
+    const newAbout = req.body
+    res.json({
+        message: "Success"
+    })
+});
+// Middleware
+app.use(express.json());
+
+
+
